1f2ee0e0159271a4ed906a984dab68e4bda8d00b
SturmEnte <sturmente@proton.me>
2022-09-11 23:04:05 +0200

    Added an Internal Link component

---
+++ src/lib/InternalLink.svelte
@@ -0,0 +1,24 @@
+<script>
+export let name;
+export let targetId;
+</script>
+
+
+<a href="#{targetId}">
+    <div>{name}</div>
+</a>
+
+<style>
+a {
+    background: var(--color-one);
+    width: 20vw;
+    height: 8vh;
+    text-decoration: none;
+    color: white;
+    font-size: 1.5em;
+    border-radius: 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
